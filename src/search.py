6cb6b6b55e8129f8e28c5ad3348f2ccd46123eae
Francisco Schulz <22344801+fschlz@users.noreply.github.com>
2021-10-29 20:24:36 +0200

    function rename

--- src/search.py
+++ src/search.py
@@ -1,12 +1,27 @@
 import pandas as pd
-from typing import List
 
 
-def search_products(search_term: str, series: pd.Series, use_regex=False) -> List[str]:
+def search_simple_pattern(search_term: str, series: pd.Series, use_regex=False) -> pd.Series:
+    """simple search via built-in methods
+    regex matching possible
 
+    Args:
+        search_term (str): string you want to search for
+        series (pd.Series): data to search through
+        use_regex (bool, optional): to use regex or not. Defaults to False.
+
+    Returns:
+        pd.Series: part of series that matches the search term
+    """
     match_series = series.str.contains(pat=search_term, regex=use_regex)
 
     series_lower = series.str.lower()
     match_series_lower = series_lower.str.contains(pat=search_term, regex=use_regex)
 
-    return set(series[match_series | match_series_lower])
+    return series[match_series | match_series_lower]
+
+
+def get_ids_from_search(
+    search_series: pd.Series, id_series: pd.Series
+) -> pd.Series:
+    return id_series.iloc[search_series.index]
